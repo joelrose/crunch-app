7c9843ad4005099f4b95670a4e403ad86ec98361
Joel Rose <mail@joel-rose.de>
2021-09-22 12:31:07 +0200

    [wip] loading

--- lib/screens/loading/loading.dart
+++ lib/screens/loading/loading.dart
@@ -19,7 +19,11 @@ class _LoadingScreenState extends State<LoadingScreen>
   AuthService auth = locator<AuthService>();
 
   late AnimationController animationController;
-  late Animation<double> tween;
+  late Animation<double> heightSequence;
+  late Animation<double> widthSequence;
+  late Animation<double> borderRadiusSequence;
+
+  Tween<double> scaleTween = Tween<double>(begin: 0, end: 1);
 
   double height = 0;
   double width = 0;
@@ -29,15 +33,73 @@ class _LoadingScreenState extends State<LoadingScreen>
     super.initState();
 
     // Defining controller with animation duration of two seconds
-    animationController =
-        AnimationController(vsync: this, duration: Duration(milliseconds: 200));
-    tween = Tween<double>(begin: 0, end: 1).animate(
-        CurvedAnimation(parent: animationController, curve: Curves.easeIn));
+    animationController = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 1000));
+
+    heightSequence = TweenSequence<double>(
+      <TweenSequenceItem<double>>[
+        TweenSequenceItem<double>(
+          tween: Tween<double>(begin: 0, end: 0.6)
+              .chain(CurveTween(curve: Curves.ease)),
+          weight: 50.0,
+        ),
+        TweenSequenceItem<double>(
+          tween: ConstantTween<double>(0.6),
+          weight: 20.0,
+        ),
+        TweenSequenceItem<double>(
+          tween: Tween<double>(begin: 0.6, end: 0.55)
+              .chain(CurveTween(curve: Curves.ease)),
+          weight: 10.0,
+        ),
+        TweenSequenceItem<double>(
+          tween: Tween<double>(begin: 0.55, end: 1)
+              .chain(CurveTween(curve: Curves.ease)),
+          weight: 20.0,
+        ),
+      ],
+    ).animate(animationController);
+
+    widthSequence = TweenSequence<double>(
+      <TweenSequenceItem<double>>[
+        TweenSequenceItem<double>(
+          tween: Tween<double>(begin: 0, end: 1)
+              .chain(CurveTween(curve: Curves.ease)),
+          weight: 20.0,
+        ),
+        TweenSequenceItem<double>(
+          tween: ConstantTween<double>(1),
+          weight: 80.0,
+        ),
+      ],
+    ).animate(animationController);
+
+    borderRadiusSequence = TweenSequence<double>(
+      <TweenSequenceItem<double>>[
+        TweenSequenceItem<double>(
+          tween: Tween<double>(begin: 0, end: 1)
+              .chain(CurveTween(curve: Curves.ease)),
+          weight: 50.0,
+        ),
+        TweenSequenceItem<double>(
+          tween: ConstantTween<double>(1),
+          weight: 30.0,
+        ),
+        TweenSequenceItem<double>(
+          tween: Tween<double>(begin: 1, end: 0)
+              .chain(CurveTween(curve: Curves.ease)),
+          weight: 20.0,
+        ),
+      ],
+    ).animate(animationController);
+
     animationController.addListener(() {
       setState(() {});
     });
 
     _loadWidget();
+
+    animationController.forward();
 
     // Future.delayed(const Duration(seconds: 1)).then(
     //   (value) => setState(() {
@@ -48,7 +110,7 @@ class _LoadingScreenState extends State<LoadingScreen>
   }
 
   Future<Timer> _loadWidget() async {
-    const _duration = Duration(seconds: 5);
+    const _duration = Duration(seconds: 100);
     return Timer(_duration, navigationPage);
   }
 
@@ -70,25 +132,33 @@ class _LoadingScreenState extends State<LoadingScreen>
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
       body: SizedBox(
         height: MediaQuery.of(context).size.height,
         child: Stack(
           children: <Widget>[
             Positioned.fill(
+              bottom: -40,
               child: Align(
                 alignment: Alignment.bottomCenter,
-                child: AnimatedContainer(
-                  alignment: Alignment.bottomCenter,
-                  decoration: const BoxDecoration(
-                    color: AlpacaColor.primary100,
-                    // borderRadius: BorderRadius.vertical(
-                    //   top: Radius.circular(100),
-                    // ),
-                  ),
-                  height: MediaQuery.of(context).size.height,
-                  width: MediaQuery.of(context).size.width,
-                  duration: const Duration(milliseconds: 200),
+                child: AnimatedBuilder(
+                  builder: (BuildContext context, Widget? child) {
+                    return Container(
+                      height: size.height * 0.7,
+                      width: widthSequence.value * (size.width * 2),
+                      alignment: Alignment.bottomCenter,
+                      decoration: const BoxDecoration(
+                        color: AlpacaColor.primary100,
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(
+                            250,
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                  animation: animationController,
                 ),
               ),
             ),
@@ -101,6 +171,27 @@ class _LoadingScreenState extends State<LoadingScreen>
                 ),
               ),
             ),
+            // Positioned.fill(
+            //   bottom: -MediaQuery.of(context).size.height,
+            //   child: Align(
+            //     alignment: Alignment.bottomCenter,
+            //     child: Container(
+            //       height: sizeSequence.value *
+            //           2 *
+            //           MediaQuery.of(context).size.height,
+            //       width: 60,
+            //       alignment: Alignment.bottomCenter,
+            //       decoration: const BoxDecoration(
+            //         color: AlpacaColor.blackColor,
+            //         borderRadius: BorderRadius.all(
+            //           Radius.circular(
+            //             50,
+            //           ),
+            //         ),
+            //       ),
+            //     ),
+            //   ),
+            // ),
           ],
         ),
       ),
