a5bd439fecd3bdbb5c98e157dbfd37e0d430da13
Lennart <Lennart.Stachowiak@code.berlin>
2021-09-27 14:59:01 +0200

    updated the NavBar removing by scrolling

--- lib/screens/home/widgets/floating_search_bar.dart
+++ lib/screens/home/widgets/floating_search_bar.dart
@@ -2,6 +2,8 @@ import 'package:alpaca/global.dart';
 import 'package:flutter/material.dart';
 import 'package:material_floating_search_bar/material_floating_search_bar.dart';
 import 'package:alpaca/screens/home/base/discover.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:alpaca/routes.dart';
 
 class SearchBar extends StatefulWidget {
   const SearchBar({Key? key}) : super(key: key);
@@ -76,131 +78,135 @@ class _SearchBarState extends State<SearchBar> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        AnimatedContainer(
-          duration: const Duration(milliseconds: 150),
-          height: isSearchBarVisible ? 0 : 60,
-          child: Wrap(children: const [DiscoverNavBar()]),
-        ),
-        Expanded(
-          child: FloatingSearchBar(
-            backgroundColor: AlpacaColor.lightGreyColor80,
-            backdropColor: AlpacaColor.white100Color,
-            shadowColor: Colors.transparent,
-            iconColor: AlpacaColor.darkGreyColor,
-            queryStyle: const TextStyle(color: AlpacaColor.blackColor),
-            scrollPadding: const EdgeInsets.only(top: 16, bottom: 56),
-            controller: controller,
-            transition: CircularFloatingSearchBarTransition(),
-            hint: 'Search for food, stores or tags...',
-            automaticallyImplyBackButton: false,
-            leadingActions: [
-              FloatingSearchBarAction.searchToClear(),
-            ],
-            actions: [
-              // FloatingSearchBarAction.back(),
-              // FloatingSearchBarAction.icon(
-              //   icon: Icons.close,
-              //   onTap: () {
-              //     controller.clear();
-              //   },
-              //   showIfClosed: false,
-              //   showIfOpened: true,
-              // ),
-              FloatingSearchBarAction(
-                showIfOpened: true,
-                showIfClosed: false,
-                child: TextButton(
-                  child: const Text(
-                    'Cancel',
-                    style: TextStyle(color: AlpacaColor.darkGreyColor),
-                  ),
-                  onPressed: () {
-                    controller.close();
+    return NestedScrollView(
+      headerSliverBuilder: (context, innerBoxIsScrolled) => [
+        SliverAppBar(
+          toolbarHeight: isSearchBarVisible ? 0 : 60,
+          backgroundColor: AlpacaColor.white100Color,
+          flexibleSpace: AnimatedContainer(
+            duration: const Duration(milliseconds: 150),
+            height: isSearchBarVisible ? 0 : 60,
+            child: Wrap(children: const [DiscoverNavBar()]),
+          ),
+        )
+      ],
+      body: Expanded(
+        child: FloatingSearchBar(
+          backgroundColor: AlpacaColor.lightGreyColor80,
+          backdropColor: AlpacaColor.white100Color,
+          shadowColor: Colors.transparent,
+          iconColor: AlpacaColor.darkGreyColor,
+          queryStyle: const TextStyle(color: AlpacaColor.blackColor),
+          scrollPadding: const EdgeInsets.only(top: 16, bottom: 56),
+          controller: controller,
+          transition: CircularFloatingSearchBarTransition(),
+          hint: 'Search for food, stores or tags...',
+          automaticallyImplyBackButton: false,
+          leadingActions: [
+            FloatingSearchBarAction.searchToClear(),
+          ],
+          actions: [
+            // FloatingSearchBarAction.back(),
+            // FloatingSearchBarAction.icon(
+            //   icon: Icons.close,
+            //   onTap: () {
+            //     controller.clear();
+            //   },
+            //   showIfClosed: false,
+            //   showIfOpened: true,
+            // ),
+            FloatingSearchBarAction(
+              showIfOpened: true,
+              showIfClosed: false,
+              child: TextButton(
+                child: const Text(
+                  'Cancel',
+                  style: TextStyle(color: AlpacaColor.darkGreyColor),
+                ),
+                onPressed: () {
+                  controller.close();
+                },
+              ),
+            )
+          ],
+          onQueryChanged: (query) {
+            setState(() {
+              filteredSearchHistory = filterSearchTerms(filter: query);
+            });
+          },
+          onFocusChanged: (v) {
+            setState(() {
+              isSearchBarVisible = !isSearchBarVisible;
+            });
+          },
+          onSubmitted: (query) {
+            setState(() {
+              addSearchTerm(query);
+              selectedTerm = query;
+            });
+            controller.close();
+          },
+          builder: (context, transition) {
+            return ClipRRect(
+              borderRadius: BorderRadius.circular(8),
+              child: Material(
+                color: AlpacaColor.white100Color,
+                child: Builder(
+                  builder: (context) {
+                    if (filteredSearchHistory!.isEmpty &&
+                        controller.query.isEmpty) {
+                      return Container(
+                        height: 56,
+                        width: double.infinity,
+                        alignment: Alignment.center,
+                        child: Text(
+                          'Start searching',
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                          style: Theme.of(context).textTheme.bodyText1,
+                        ),
+                      );
+                    } else if (filteredSearchHistory!.isEmpty) {
+                      return ListTile(
+                        title: Text(controller.query),
+                        onTap: () {
+                          setState(() {
+                            addSearchTerm(controller.query);
+                            selectedTerm = controller.query;
+                          });
+                          controller.close();
+                        },
+                      );
+                    } else {
+                      return Container();
+                      // return Column(
+                      //   mainAxisSize: MainAxisSize.min,
+                      //   children: filteredSearchHistory.map(
+                      //     (term) => ListTile(
+                      //     title: Text(
+                      //       term,
+                      //       maxLines: 1,
+                      //       overflow: TextOverflow.ellipsis,
+                      //       ),
+                      //     ),
+                      //   ),
+                      // );
+                    }
                   },
                 ),
-              )
-            ],
-            onQueryChanged: (query) {
-              setState(() {
-                filteredSearchHistory = filterSearchTerms(filter: query);
-              });
-            },
-            onFocusChanged: (v) {
-              setState(() {
-                isSearchBarVisible = !isSearchBarVisible;
-              });
-            },
-            onSubmitted: (query) {
-              setState(() {
-                addSearchTerm(query);
-                selectedTerm = query;
-              });
-              controller.close();
-            },
-            builder: (context, transition) {
-              return ClipRRect(
-                borderRadius: BorderRadius.circular(8),
-                child: Material(
-                  color: AlpacaColor.white100Color,
-                  child: Builder(
-                    builder: (context) {
-                      if (filteredSearchHistory!.isEmpty &&
-                          controller.query.isEmpty) {
-                        return Container(
-                          height: 56,
-                          width: double.infinity,
-                          alignment: Alignment.center,
-                          child: Text(
-                            'Start searching',
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
-                            style: Theme.of(context).textTheme.bodyText1,
-                          ),
-                        );
-                      } else if (filteredSearchHistory!.isEmpty) {
-                        return ListTile(
-                          title: Text(controller.query),
-                          onTap: () {
-                            setState(() {
-                              addSearchTerm(controller.query);
-                              selectedTerm = controller.query;
-                            });
-                            controller.close();
-                          },
-                        );
-                      } else {
-                        return Container();
-                        // return Column(
-                        //   mainAxisSize: MainAxisSize.min,
-                        //   children: filteredSearchHistory.map(
-                        //     (term) => ListTile(
-                        //     title: Text(
-                        //       term,
-                        //       maxLines: 1,
-                        //       overflow: TextOverflow.ellipsis,
-                        //       ),
-                        //     ),
-                        //   ),
-                        // );
-                      }
-                    },
-                  ),
-                ),
-              );
-            },
-            body: SingleChildScrollView(
-              physics: const AlwaysScrollableScrollPhysics(),
-              child: Column(
-                children: const [
-                  DiscoverBody(),
-                ],
               ),
+            );
+          },
+          body: SingleChildScrollView(
+            physics: const AlwaysScrollableScrollPhysics(),
+            child: Column(
+              children: const [
+                DiscoverBody(),
+              ],
             ),
           ),
         ),
-      ],
+      ),
     );
   }
 }
