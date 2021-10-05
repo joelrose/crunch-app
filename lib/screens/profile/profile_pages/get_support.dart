028c70767b4f5f34560320d98f5f32b4acf5008c
Lennart <Lennart.Stachowiak@code.berlin>
2021-10-05 13:54:42 +0200

    [change] get support side

--- lib/screens/profile/profile_pages/get_support.dart
+++ lib/screens/profile/profile_pages/get_support.dart
@@ -1,20 +1,22 @@
 import 'package:alpaca/global.dart';
+import 'package:alpaca/shared/page_wrapper.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class GetSupport extends StatelessWidget {
   const GetSupport({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Center(
-        child: Text(
-          'Support',
-          style: Theme.of(context)
-              .textTheme
-              .headline1!
-              .merge(const TextStyle(color: AlpacaColor.darkGreyColor)),
-        ),
+    return PageWrapper(
+      backgroundColor: AlpacaColor.white100Color,
+      statusBarStyle: SystemUiOverlayStyle.dark,
+      child: Text(
+        'Support',
+        style: Theme.of(context)
+            .textTheme
+            .headline1!
+            .merge(const TextStyle(color: AlpacaColor.darkGreyColor)),
       ),
     );
   }
