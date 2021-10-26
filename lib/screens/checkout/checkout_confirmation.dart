9c4dbda145e7ce79686aa2cf74686014344c3e60
Lennart <Lennart.Stachowiak@code.berlin>
2021-10-26 18:03:46 +0200

    [add] text

--- lib/screens/checkout/checkout_confirmation.dart
+++ lib/screens/checkout/checkout_confirmation.dart
@@ -31,11 +31,32 @@ class CheckoutConfirmationScreen extends StatelessWidget {
       padding: EdgeInsets.zero,
       backgroundColor: AlpacaColor.white100Color,
       statusBarStyle: SystemUiOverlayStyle.dark,
-      child: Column(
+      child: ListView(
         children: [
           CheckoutOrderNavbarWidget(
             pageOverviewName: 'Order status',
             disableEditButton: true,
+          ),
+          Column(
+            children: [
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Container(
+                    width: 230,
+                    child: Text(
+                      'Your order is being prepared...',
+                      style: Theme.of(context)
+                          .textTheme
+                          .headline2
+                          ?.copyWith(fontSize: 22),
+                    ),
+                  ),
+                ),
+              ),
+            ],
           )
         ],
       ),
