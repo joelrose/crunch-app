5110622e5bdad35496f6b26124200317a43606a8
Lennart <Lennart.Stachowiak@code.berlin>
2021-12-10 17:42:34 +0100

    refactor image nav bar to own widget + showbutton

---
+++ apps/pickup/lib/screens/store/widgets/store_image_navbar.dart
@@ -0,0 +1,96 @@
+import 'dart:ui';
+
+import 'package:alpaca/alpaca.dart';
+import 'package:flutter/material.dart';
+
+class StoreImageNavbar extends StatelessWidget {
+  StoreImageNavbar({Key? key, required this.image, this.showButtons = true})
+      : super(key: key);
+
+  bool showButtons;
+  String image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 200,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          fit: BoxFit.cover,
+          image: Image.network(
+            image,
+          ).image,
+        ),
+      ),
+      padding: const EdgeInsets.symmetric(
+        horizontal: 18,
+        vertical: 18,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          GestureDetector(
+            onTap: () => {Navigator.of(context).pop()},
+            child: const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Icon(
+                Icons.arrow_back_ios,
+                size: 20,
+                color: AlpacaColor.white100Color,
+              ),
+            ),
+          ),
+          if (showButtons)
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(
+                    right: 10,
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(30),
+                    child: SizedBox(
+                      height: 40,
+                      width: 40,
+                      child: BackdropFilter(
+                        filter: ImageFilter.blur(
+                          sigmaX: 10,
+                          sigmaY: 10,
+                        ),
+                        child: IconButton(
+                          onPressed: () {},
+                          color: AlpacaColor.white100Color,
+                          icon: const Icon(
+                            Icons.star_border_outlined,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(30),
+                  child: SizedBox(
+                    height: 40,
+                    width: 40,
+                    child: BackdropFilter(
+                      filter: ImageFilter.blur(
+                        sigmaX: 10,
+                        sigmaY: 10,
+                      ),
+                      child: IconButton(
+                        onPressed: () {},
+                        color: AlpacaColor.white100Color,
+                        icon: const Icon(Icons.info_outline),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+        ],
+      ),
+    );
+  }
+}
