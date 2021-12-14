e7508aa04472e683cb62fb6cbcf56f551e005e07
Joel Rose <mail@joel-rose.de>
2021-12-14 12:23:01 +0100

    [add] shell file to generate swagger files automatically

--- scripts/swagger.sh
+++ scripts/swagger.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
-mkdir swaggers
+cd ../core/hermes_api
 
-cd swaggers
+curl http://localhost:5000/swagger/v1/swagger.json --output lib/swagger.json
 
-curl -O https://localhost:5001/swagger/v1/swagger.json
-
-rm -R ../lib/swagger
+rm -R lib/swagger_generated_code
 
 flutter pub run build_runner build
 
-rm -R ../swaggers
+rm lib/swagger.json
