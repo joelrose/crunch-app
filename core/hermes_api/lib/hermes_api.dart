4bf1f2529bcadb0c09d3293937e7911bab2700a2
Joel Rose <mail@joel-rose.de>
2021-12-14 11:50:51 +0100

    [add] custom json serializer for chopper

--- core/hermes_api/lib/hermes_api.dart
+++ core/hermes_api/lib/hermes_api.dart
@@ -2,3 +2,4 @@ library hermes_api;
 
 export 'package:chopper/chopper.dart';
 export 'package:hermes_api/swagger_generated_code/swagger.swagger.dart';
+export 'package:hermes_api/src/json_serializer.dart';
