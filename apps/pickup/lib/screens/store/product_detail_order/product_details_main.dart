f3d3faf5c5677b9afd4c5270edc82a4c8434bf17
Lennart <Lennart.Stachowiak@code.berlin>
2022-01-10 16:13:14 +0100

    refactor product details order page

---
+++ apps/pickup/lib/screens/store/product_detail_order/product_details_main.dart
@@ -0,0 +1,62 @@
+import 'package:alpaca/alpaca.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:pickup/screens/store/widgets/store_image_navbar.dart';
+import 'package:sanity/sanity.dart';
+
+import '../widgets/store_menue_list.dart';
+import 'product_amount_and_add_to_order.dart';
+import 'product_detail_text.dart';
+import 'product_radio_checkbox.dart';
+
+class StoreProductOverview extends StatefulWidget {
+  const StoreProductOverview({Key? key, required this.data}) : super(key: key);
+  final ProductDetailsData data;
+
+  @override
+  _StoreProductOverviewState createState() => _StoreProductOverviewState();
+}
+
+class _StoreProductOverviewState extends State<StoreProductOverview> {
+  @override
+  Widget build(BuildContext context) {
+    return PageWrapper(
+      statusBarStyle: SystemUiOverlayStyle.dark,
+      padding: EdgeInsets.zero,
+      backgroundColor: AlpacaColor.white100Color,
+      child: Column(
+        children: [
+          Expanded(
+            child: CustomScrollView(
+              shrinkWrap: true,
+              slivers: [
+                SliverList(
+                  delegate: SliverChildListDelegate(
+                    [
+                      StoreImageNavbar(
+                        image: widget.data.restaurantImage,
+                        showButtons: false,
+                      ),
+                      ProductBasicDetails(
+                        title: widget.data.item.title.english,
+                      ),
+                      ProductRadioCheckbox(
+                        itemCategories: widget.data.itemOptions,
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          ProductAmountAndAddToOrder(
+            data: widget.data,
+          )
+        ],
+      ),
+    );
+  }
+}
+
+
---
+++ apps/pickup/lib/screens/store/product_detail_order/product_radio_checkbox.dart
@@ -0,0 +1,124 @@
+import 'package:alpaca/alpaca.dart';
+import 'package:flutter/material.dart';
+import 'package:sanity/sanity.dart';
+
+class ProductRadioCheckbox extends StatefulWidget {
+  const ProductRadioCheckbox({Key? key, required this.itemCategories})
+      : super(key: key);
+  final List<RestaurantMenueItemOptions>? itemCategories;
+
+  @override
+  _ProductRadioCheckboxState createState() => _ProductRadioCheckboxState();
+}
+
+class _ProductRadioCheckboxState extends State<ProductRadioCheckbox> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      physics: const NeverScrollableScrollPhysics(),
+      shrinkWrap: true,
+      itemCount: widget.itemCategories?.length ?? 0,
+      itemBuilder: (context, i) {
+        final item = widget.itemCategories![i];
+        var choice = item.options[0].id;
+        return StatefulBuilder(
+          builder: (BuildContext context, StateSetter setState) {
+            return Container(
+              margin: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    item.title.english,
+                    style: Theme.of(context).textTheme.headline2,
+                  ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(vertical: 20),
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        width: 0.5,
+                        color: AlpacaColor.greyColor,
+                      ),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: ListView.separated(
+                      itemCount: item.options.length,
+                      physics: const NeverScrollableScrollPhysics(),
+                      shrinkWrap: true,
+                      separatorBuilder: (context, index) {
+                        return const Divider(
+                          height: 0,
+                        );
+                      },
+                      itemBuilder: (context, index) => GestureDetector(
+                        behavior: HitTestBehavior.translucent,
+                        onTap: () {
+                          setState(() {
+                            choice = item.options[index].id;
+                          });
+                        },
+                        child: Row(
+                          children: [
+                            Radio(
+                              value: item.options[index].id,
+                              groupValue: choice,
+                              onChanged: (value) {
+                                setState(() {
+                                  choice = value.toString();
+                                });
+                              },
+                              activeColor: AlpacaColor.primary100,
+                            ),
+                            Expanded(
+                              child: Padding(
+                                padding: const EdgeInsets.only(right: 17),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      item.options[index].title.english,
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .subtitle1!
+                                          .merge(
+                                            const TextStyle(
+                                              color: AlpacaColor.darkNavyColor,
+                                              fontWeight: FontWeight.w600,
+                                              fontSize: 13,
+                                            ),
+                                          ),
+                                    ),
+                                    if (item.options[index].price != 0)
+                                      Text(
+                                        '+ ${item.options[index].price.toString()}€',
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .subtitle1!
+                                            .merge(
+                                              const TextStyle(
+                                                color: AlpacaColor.primary100,
+                                                fontWeight: FontWeight.w600,
+                                                fontSize: 12,
+                                              ),
+                                            ),
+                                      ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+}
