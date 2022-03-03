e0ae97fcdc2b2486a4fa441edca3003a2f511f51
Jonas Wallmann <jonas.wallmann@code.berlin>
2022-03-03 19:25:31 +0100

    refactor: checkout folder structure

--- lib/screens/checkout_cart_items/checkout_cart_items.dart
+++ lib/screens/checkout_cart_items/checkout_cart_items.dart
@@ -1,90 +1,10 @@
 import 'package:alpaca/alpaca.dart';
 import 'package:flutter/material.dart';
+import 'package:pickup/screens/checkout/models/check_out_item_amount.dart';
+import 'package:pickup/screens/checkout/models/check_out_vm.dart';
 import 'package:pickup/screens/checkout/widgets/checkout_main_widget.dart';
 import 'package:pickup/shared/utilities.dart';
 import 'package:sanity/sanity.dart';
-
-class CheckoutItemAmount {
-  CheckoutItemAmount({
-    required this.checkoutItem,
-    required this.amount,
-    required this.totalPrice,
-  });
-  CheckoutItemModel checkoutItem;
-  int amount;
-  double totalPrice;
-}
-
-class CheckOutVM {
-  // TASK: Sumarise the checkout items by grouping same items and counting them
-
-  CheckOutVM(List<CheckoutItemModel> checkoutItems) {
-    groupItems(checkoutItems);
-  }
-
-  //each induvidual item with count
-  List<CheckoutItemAmount> checkoutSummaryList = [];
-
-  void groupItems(List<CheckoutItemModel> checkoutItems) {
-    for (final item in checkoutItems) {
-      if (!alreadyInList(checkoutSummaryList, item)) {
-        // calculate
-        final int amount = countInList(checkoutItems, item);
-        final double itemPrice = calculateItemPrice(item);
-        final double totalPrice = itemPrice * amount;
-        // add
-        checkoutSummaryList.add(
-          CheckoutItemAmount(
-            checkoutItem: item,
-            amount: amount,
-            totalPrice: totalPrice,
-          ),
-        );
-      }
-    }
-  }
-
-  double calculateItemPrice(CheckoutItemModel item) {
-    double itemAddOnPrice = 0;
-    for (final itemOption in item.itemOptions) {
-      itemAddOnPrice += itemOption.option.price;
-    }
-    return item.price.toDouble() + itemAddOnPrice;
-  }
-
-  bool alreadyInList(
-    List<CheckoutItemAmount> summaryList,
-    CheckoutItemModel item,
-  ) {
-    if (summaryList.isEmpty) {
-      return false;
-    }
-    for (final summaryItem in summaryList) {
-      if (sameItem(summaryItem.checkoutItem, item)) {
-        // if (identical(summaryItem.checkoutItem, item)) {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  bool sameItem(CheckoutItemModel item1, CheckoutItemModel item2) {
-    return item1.id == item2.id; //&& item1.itemOptions == item2.itemOptions;
-  }
-
-  int countInList(
-    List<CheckoutItemModel> checkoutList,
-    CheckoutItemModel item,
-  ) {
-    int count = 0;
-    for (final itemInList in checkoutList) {
-      if (itemInList.id == item.id) {
-        count += 1;
-      }
-    }
-    return count;
-  }
-}
 
 class CheckoutCartItemsWidget extends StatelessWidget {
   const CheckoutCartItemsWidget({Key? key, required this.checkoutItems})
