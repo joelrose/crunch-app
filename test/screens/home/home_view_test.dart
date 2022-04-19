1700f8084c8c52eb6b40113c1aedd06a1be65bf0
Joel Rose <mail@joel-rose.de>
2022-04-19 15:43:15 +0200

    test: mock discover cubit

--- test/screens/home/home_view_test.dart
+++ test/screens/home/home_view_test.dart
@@ -1,6 +1,9 @@
+import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:hermes_repository/hermes_repository.dart';
+import 'package:mockingjay/mockingjay.dart';
 import 'package:mocktail_image_network/mocktail_image_network.dart';
 import 'package:pickup/screens/discover/cubit/discover_cubit.dart';
 import 'package:pickup/screens/discover/discover.dart';
@@ -11,20 +14,34 @@ import 'package:pickup/screens/orders/orders.dart';
 import '../../helpers/pump_app.dart';
 import '../../mock/mock_hermes_service.dart';
 
+class MockDiscoverCubit extends MockCubit<DiscoverState>
+    implements DiscoverCubit {}
+
 void main() {
+  late DiscoverCubit discoverCubit;
+
   group('App Bottom Nav WidgetTest', () {
+    setUp(() {
+      discoverCubit = MockDiscoverCubit();
+      when(() => discoverCubit.state).thenReturn(
+        DiscoverState(
+          status: DiscoverStatus.success,
+          stores: [],
+        ),
+      );
+    });
+
     testWidgets('Testing Bottom Navigation Bar loaded', (tester) async {
       await mockNetworkImages(() async {
         await tester.pumpApp(
-          BlocProvider(
-            create: (_) => DiscoverCubit(
-              MockHermesService('[]', 200),
-            ),
+          BlocProvider.value(
+            value: discoverCubit,
             child: BlocProvider(
               create: (_) => HomeCubit(),
               child: HomeView(),
             ),
           ),
+          hermesRepository: MockHermesService('[]', 200),
         );
 
         await tester.pumpAndSettle();
