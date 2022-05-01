58c18e7fb1f60eac17516027f8d74a68320de799
Joel Rose <mail@joel-rose.de>
2022-05-01 23:16:00 +0200

    feat: add local storage api repository

---
+++ packages/local_storage_checkout_api/lib/local_storage_checkout_api.dart
@@ -0,0 +1,9 @@
+library local_storage_checkout_api;
+
+export 'package:shared_preferences/shared_preferences.dart'
+    show SharedPreferences;
+
+export 'package:hermes_repository/hermes_repository.dart'
+    show CreateOrderItemDto;
+
+export 'src/local_storage_checkout_api.dart';
