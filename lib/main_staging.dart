199f0f59c906c31878064abd62d631ff8045ad47
Joel Rose <mail@joel-rose.de>
2022-05-01 23:28:01 +0200

    refactor: move app view & add checkout basket bloc

--- lib/main_staging.dart
+++ lib/main_staging.dart
@@ -1,7 +1,66 @@
-import 'package:pickup/bootstrap.dart';
-import 'package:pickup/main_app.dart';
-import 'package:pickup/shared/enum/enviroment.dart';
+import 'dart:async';
 
-void main() {
-  bootstrap(() => const App(), Enviroment.staging);
+import 'package:authentication_repository/authentication_repository.dart';
+import 'package:checkout_repository/checkout_repository.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:hermes_repository/hermes_repository.dart';
+import 'package:local_storage_checkout_api/local_storage_checkout_api.dart';
+import 'package:onesignal_repository/onesignal_repository.dart';
+import 'package:pickup/screens/app/app.dart';
+import 'package:pickup/shared/enum/enviroment.dart';
+import 'package:stripe_repository/stripe_repository.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+
+  await dotenv.load(
+    fileName:
+        'assets/enviroments/.env.${Enviroment.staging.toString().split('.').last}',
+  );
+
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+
+  final _authenticationRepository = AuthenticationRepository();
+
+  final _oneSignalRepository = OneSignalRepository(
+    authenticationRepository: _authenticationRepository,
+  );
+
+  final _hermesRepository = HermesRepository(
+    apiUrl: dotenv.get('API_URL'),
+    authenticationRepository: _authenticationRepository,
+  );
+
+  final _stripeRepository = StripeRepository(
+    hermesRepository: _hermesRepository,
+  );
+
+  final _checkoutRepository = CheckoutRepository(
+    checkoutApi:
+        LocalStorageCheckoutApi(plugin: await SharedPreferences.getInstance()),
+  );
+
+  _stripeRepository.setupStripe(
+    merchantIdentifier: dotenv.env['STRIPE_MERCHANT_IDENTIFIER']!,
+    stripeKey: dotenv.env['STRIPE_KEY']!,
+  );
+
+  runZonedGuarded(
+    () async => runApp(
+      App(
+        authenticationRepository: _authenticationRepository,
+        checkoutRepository: _checkoutRepository,
+        oneSignalRepository: _oneSignalRepository,
+        hermesRepository: _hermesRepository,
+        stripeRepository: _stripeRepository,
+      ),
+    ),
+    (error, stackTrace) =>
+        FirebaseCrashlytics.instance.recordError(error, stackTrace),
+  );
 }
