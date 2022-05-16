956a315cb7733c1e448692dc3520316ffa24fd14
Florian <61353423+FlorianGrollich@users.noreply.github.com>
2022-05-16 14:17:15 +0200

    fix: simplified and seperated logic to calculate the opening hours

--- lib/screens/checkout/cubits/checkout_timer_cubit/checkout_time_cubit.dart
+++ lib/screens/checkout/cubits/checkout_timer_cubit/checkout_time_cubit.dart
@@ -9,60 +9,19 @@ part 'checkout_time_state.dart';
 
 part 'checkout_time_cubit.freezed.dart';
 
+typedef ListIndex = int Function(int);
+
 class CheckoutTimeCubit extends Cubit<CheckoutTimeState> {
   factory CheckoutTimeCubit({
     required DateTime currentTime,
-    required List<DeliverectAvailabilityModel>? openingHours,
+    required List<DeliverectAvailabilityModel> openingHours,
   }) {
-    final List<OpeningHour> availableOpeningTimes = [];
+    openingHours.removeWhere(
+      (element) => element.dayOfWeek!.index != currentTime.weekday,
+    );
 
-    openingHours!.removeWhere(
-        (element) => element.dayOfWeek!.index != currentTime.weekday);
-
-    for (final e in openingHours) {
-      final startTime = e.startTime;
-      final startingHour = startTime!.getHour;
-      final startingMinute = startTime.getMinutes;
-
-      availableOpeningTimes.add(
-        OpeningHour(
-          startingHour,
-          List.generate(
-            60 - startingMinute,
-            (index) => index + startingMinute,
-          ),
-        ),
-      );
-
-      final endTime = e.endTime;
-      final endingHour = endTime!.getHour;
-      final endingMinute = endTime.getMinutes;
-      availableOpeningTimes.add(
-        OpeningHour(
-          endingHour,
-          List.generate(
-            endingMinute != 0 ? 60 - (60 - endingMinute) : 1,
-            (index) => endingMinute - index,
-          ),
-        ),
-      );
-
-      if (e.endTime != null && e.startTime != null) {
-        final startingHour = int.parse(e.startTime!.substring(0, 2));
-        final endingHour = int.parse(e.endTime!.substring(0, 2));
-        final allHoursBetween = List.generate(
-          endingHour - startingHour - 1,
-          (index) => index + startingHour + 1,
-        );
-        for (final e in allHoursBetween) {
-          availableOpeningTimes
-              .add(OpeningHour(e, List.generate(60, (index) => index)));
-        }
-      }
-    }
+    final availableOpeningTimes = openingHours.getOpeningHours;
     availableOpeningTimes.sort((a, b) => a.hour.compareTo(b.hour));
-
-    availableOpeningTimes.removeWhere((e) => e.minutes.length == 1);
 
     return CheckoutTimeCubit._internal(
       pickupTime: currentTime.add(const Duration(minutes: 20)),
@@ -86,24 +45,12 @@ class CheckoutTimeCubit extends Cubit<CheckoutTimeState> {
           ),
         );
 
-  void updatePickupTime() {
-    final today = DateTime.now();
-    final newTime = DateTime(
-      today.year,
-      today.month,
-      today.day,
-      state.currentSelectedHour.hour,
-      state.currentSelectedHour.minutes[state.currentSelectedMinuteIndex],
-    );
-    emit(state.copyWith(pickupTime: newTime));
-  }
-
-  void updateCurrentSelectedMinute(int minIndex) {
+  void updateCurrentSelectedMinute(int minuteIndex) {
     emit(
       state.copyWith(
-        currentSelectedMinuteIndex: minIndex,
+        currentSelectedMinuteIndex: minuteIndex,
         pickupTime: state.pickupTime
-            .copyWith(minute: state.currentSelectedHour.minutes[minIndex]),
+            .copyWith(minute: state.currentSelectedHour.minutes[minuteIndex]),
       ),
     );
   }
@@ -114,9 +61,16 @@ class CheckoutTimeCubit extends Cubit<CheckoutTimeState> {
       state.currentSelectedHour.minutes[state.currentSelectedMinuteIndex],
     );
 
-    if (newHour.minutes.contains(
-      state.currentSelectedHour.minutes[state.currentSelectedMinuteIndex],
-    )) {
+    if (newMinuteIndex == -1) {
+      emit(
+        state.copyWith(
+          currentSelectedHour: newHour,
+          currentSelectedMinuteIndex: 0,
+          pickupTime: state.pickupTime
+              .copyWith(hour: newHour.hour, minute: newHour.minutes[0]),
+        ),
+      );
+    } else {
       emit(
         state.copyWith(
           pickupTime: state.pickupTime.copyWith(
@@ -126,15 +80,6 @@ class CheckoutTimeCubit extends Cubit<CheckoutTimeState> {
           ),
           currentSelectedHour: newHour,
           currentSelectedMinuteIndex: newMinuteIndex,
-        ),
-      );
-    } else {
-      emit(
-        state.copyWith(
-          currentSelectedHour: newHour,
-          currentSelectedMinuteIndex: 0,
-          pickupTime: state.pickupTime
-              .copyWith(hour: newHour.hour, minute: newHour.minutes[0]),
         ),
       );
     }
--- lib/screens/checkout/util/extensions.dart
+++ lib/screens/checkout/util/extensions.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:hermes_repository/hermes_repository.dart';
 import 'package:intl/intl.dart';
+import 'package:pickup/screens/checkout/domain/entities/opening_hour.dart';
 
 extension DateFormatter on DateTime {
   String formatToLocalTime(BuildContext context) {
@@ -16,5 +18,48 @@ extension GetTimeFromString on String {
 
   int get getMinutes {
     return int.parse(substring(3, 5));
+  }
+}
+
+extension GetOpeningHourFromInt on int {
+  OpeningHour get toOpeningHour {
+    return OpeningHour(this, List.generate(60, (index) => index));
+  }
+}
+
+extension GetOpeningHours on String {
+  OpeningHour get fromEndingHour {
+    return OpeningHour(
+      getHour,
+      List.generate(getMinutes, (index) => getMinutes - index),
+    );
+  }
+
+  OpeningHour get fromStartingHour {
+    return OpeningHour(
+      getHour,
+      List.generate(
+        60 - getMinutes,
+        (index) => index + getMinutes,
+      ),
+    );
+  }
+}
+
+extension GetListOfOpeningHoures on List<DeliverectAvailabilityModel> {
+  List<OpeningHour> get getOpeningHours {
+    final List<OpeningHour> openingHours = [];
+    for (final e in this) {
+      openingHours.add(e.startTime!.fromStartingHour);
+      if (e.endTime!.getMinutes != 0) {
+        openingHours.add(e.endTime!.fromEndingHour);
+      }
+
+      for (int i = e.startTime!.getHour + 1; i < e.endTime!.getHour; i++) {
+        openingHours.add(i.toOpeningHour);
+      }
+    }
+
+    return openingHours;
   }
 }
