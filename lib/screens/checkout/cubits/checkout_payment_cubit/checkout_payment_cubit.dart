1a9f2d580f0110074b286afb437a5dcf8e4197ab
Florian <61353423+FlorianGrollich@users.noreply.github.com>
2022-05-16 14:14:50 +0200

    test: checkout payment cubit

--- lib/screens/checkout/cubits/checkout_payment_cubit/checkout_payment_cubit.dart
+++ lib/screens/checkout/cubits/checkout_payment_cubit/checkout_payment_cubit.dart
@@ -30,8 +30,6 @@ class CheckoutPaymentCubit extends Cubit<CheckoutPaymentState> {
 
       emit(const CheckoutPaymentState(PaymentStatus.paid));
     } catch (exception, stack) {
-      FirebaseCrashlytics.instance.recordError(exception, stack);
-
       emit(const CheckoutPaymentState(PaymentStatus.failed));
     }
   }
