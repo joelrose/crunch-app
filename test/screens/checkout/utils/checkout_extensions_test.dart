0961d04aeba1e64451418df0d731f81a6eed1aba
Florian <61353423+FlorianGrollich@users.noreply.github.com>
2022-05-18 20:55:54 +0200

    test: add test for local time

--- test/screens/checkout/utils/checkout_extensions_test.dart
+++ test/screens/checkout/utils/checkout_extensions_test.dart
@@ -1,13 +1,25 @@
 import 'dart:math';
 
 import 'package:flutter_test/flutter_test.dart';
+import 'package:hermes_repository/hermes_repository.dart';
 import 'package:pickup/screens/checkout/checkout.dart';
 import 'package:pickup/screens/checkout/domain/entities/opening_hour.dart';
+import 'package:pickup/screens/checkout/util/helper.dart';
+
+import '../view/checkout_page_test.dart';
 
 void main() {
   const mockTimeString = '12:05';
 
   final mockCurrentTime = DateTime(2022, 5, 12, 9, 55);
+
+  final mockOpeningHours = [
+    DeliverectAvailabilityModel(
+      startTime: '09:00',
+      endTime: '12:00',
+      dayOfWeek: DeliverectDay.thursday,
+    ),
+  ];
 
   group('check if creating the opening Houres works', () {
     test('check if creation of Opening hour from current time works', () {
@@ -25,20 +37,26 @@ void main() {
   group('check if extracting hours and minutes work', () {
     test('if extracting minutes from String', () {
       final minutes = mockTimeString.getMinutes;
-
       expect(
         minutes,
-        equals(30),
+        equals(5),
       );
+    });
+
+    test('check if extension DateTime to local time works', () {
+      final mockDate =  DateTime(2022, 10, 12, 12, 30);
+      final formatted =mockDate.formatToLocalTime('de');
+      expect(formatted, "12:30");
     });
 
     test('if extracting houres from String', () {
       final hour = mockTimeString.getHour;
-
       expect(
         hour,
         equals(12),
       );
     });
   });
+
+
 }
