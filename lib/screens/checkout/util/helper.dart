70bb357bcb5d0a8f8c1e8a3fc0e263d437c2ec74
Florian <61353423+FlorianGrollich@users.noreply.github.com>
2022-05-18 18:49:35 +0200

    fix: false starting point of for loop

--- lib/screens/checkout/util/helper.dart
+++ lib/screens/checkout/util/helper.dart
@@ -6,15 +6,16 @@ List<OpeningHour> getOpeningHours(
     {required DateTime currentDateTime,
     required List<DeliverectAvailabilityModel> availabilityModels}) {
   final List<OpeningHour> openingHours = [];
+  final pickUpTime = currentDateTime.add(const Duration(minutes: 20));
   for (final e in availabilityModels) {
     openingHours.add(
-      currentDateTime.add(const Duration(minutes: 20)).fromStartingHour,
+      pickUpTime.fromStartingHour,
     );
     if (e.endTime!.getMinutes != 0) {
       openingHours.add(e.endTime!.fromEndingHour);
     }
 
-    for (int i = e.startTime!.getHour + 1; i < e.endTime!.getHour; i++) {
+    for (int i = pickUpTime.hour + 1; i < e.endTime!.getHour; i++) {
       openingHours.add(i.toOpeningHour);
     }
   }
