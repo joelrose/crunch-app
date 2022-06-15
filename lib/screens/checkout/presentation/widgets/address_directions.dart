11b306eab115cbc80589fe270041ec507ffebe13
Joel Rose <mail@joel-rose.de>
2022-06-15 19:10:37 +0200

    fix: provider not found in checkout confirmation page

--- lib/screens/checkout/presentation/widgets/address_directions.dart
+++ lib/screens/checkout/presentation/widgets/address_directions.dart
@@ -1,25 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pickup/l10n/l10n.dart';
 import 'package:pickup/screens/checkout/checkout.dart';
 import 'package:pickup/screens/checkout_row_header/checkout_row_header.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 
 class AddressDirection extends StatelessWidget {
-  const AddressDirection({Key? key}) : super(key: key);
+  const AddressDirection({Key? key, required this.googleMapsLink})
+      : super(key: key);
+
+  final String googleMapsLink;
 
   @override
   Widget build(BuildContext context) {
-    final googleMaps = context
-        .select((CheckoutCubit cubit) => cubit.state.menu.googleMapsLink)!;
-
     return Column(
       children: [
         const DividerWidget(),
         CheckoutRowHeader(
           header: context.l10n.store,
           onPressed: () async {
-            await launchUrlString(googleMaps);
+            await launchUrlString(googleMapsLink);
           },
           buttonText: context.l10n.directionsButtonLabel,
           disableButtonBackground: true,
